4816121ac95fc1f693ff81b17d03b22367d3f0f9
duytq <duy.tran@beesightsoft.com>
2017-10-03 03:51:54 -0700

    update chat specific room

--- awesomechat/awesomechat.js
+++ awesomechat/awesomechat.js
@@ -14,7 +14,7 @@ var conn = mysql.createConnection({
 
 conn.connect(function(err) {
 	if (err) throw err.stack;
-	console.log('connect database success');
+	console.log('Connect database success');
 });
 
 // app.get('/', function(req, res){
@@ -22,44 +22,48 @@ conn.connect(function(err) {
 // });
 
 http.listen(3000, function(){
-	console.log('listening on port: 3000');
+	console.log('Server socket listening on port: 3000');
 });
 
 
 io.on('connection', function(socket) {
 
 	socket.on('disconnect', function() {
-		socket.broadcast.emit('leave_room', {
+		socket.broadcast.to(socket.room).emit('leave_room', {
 				username: socket.nickname,
    	 			message: 'leave room'
    		 	});
 		delete users[socket.nickname];
 	})
 
-	socket.on('send_message', function(content, toGroup, timestamp, typeMessage) {
-      	socket.broadcast.emit('receive_message', {
-    		from_user: socket.nickname,
+	socket.on('send_message', function(content, timestamp, typeMessage) {
+      	socket.broadcast.to(socket.room).emit('receive_message', {
+      		from_user: socket.nickname,
     		content: content,
-    		to_group: toGroup,
+    		to_group: socket.room,
     		time_arrive: timestamp,
     		type_message: typeMessage
       	});
 
-    	var sql = 'INSERT INTO archive(from_user, to_group, content, time_arrive, type_message) VALUES (' + '"' + socket.nickname + '", "' + toGroup + '", "' + content + '", "' + timestamp + '", "' + typeMessage + '")';
+    	var sql = 'INSERT INTO archive(from_user, to_group, content, time_arrive, type_message) VALUES (' + '"' + socket.nickname + '", "' + socket.room + '", "' + content + '", "' + timestamp + '", "' + typeMessage + '")';
     	
     	conn.query(sql, function(err) {
     		if (err) throw err;
         });
  	 });
 
-	socket.on('join_room', function(username) {
+	socket.on('join_room', function(username, room) {
 		if (username in users) {
+			console.log("User connect fail");
 			return;
 		} else {
+			console.log("User connected");
+			socket.room = room;
+			socket.join(socket.room);
 			socket.nickname = username;
 			users[socket.nickname] = socket;
 
-			socket.broadcast.emit('join_room', {
+			socket.broadcast.to(socket.room).emit('join_room', {
 				username: username,
    	 			message: 'joined this room'
    		 	});
